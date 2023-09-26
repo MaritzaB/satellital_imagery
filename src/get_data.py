b17c8c078bf143fb634f9514e327280c25c86650
maritzée <amby.121192@gmail.com>
2023-09-26 11:28:47 -0600

    🚧 Update api to obtain data from 2014-2017 first 120 days

--- src/get_data.py
+++ src/get_data.py
@@ -2,40 +2,33 @@ import wget
 from datetime import datetime
 from bs4 import BeautifulSoup
 
+# https://www.ncei.noaa.gov/data/oceans/ghrsst/L4/
+#
+
 def date_2_nday(date_str):
     date_object = datetime.strptime(date_str, "%Y%m%d")
     n_day = date_object.timetuple().tm_yday
     n_day_3digits = "{:03d}".format(n_day)
     return n_day_3digits
 
-print(date_2_nday('20140101'))
+def julian_2_date(year, julian_day):
+    date_object = datetime.strptime(f'{year} {julian_day}', '%Y %j')
+    return date_object.strftime('%Y%m%d')
 
-def get_url_noaa(year, month, day):
+def get_url_noaa(year, julian_day):
     folder = 'data'
     processing_level = 'L4'
-    fecha = f'{year}{"{:02d}".format(month)}{"{:02d}".format(day)}'
+    fecha = julian_2_date(year, julian_day)
     temperature_type = 'ghrsst'
     sst_type = 'MUR25'
     scale = 'GLOB'
     institution = 'JPL'
-    day = date_2_nday(fecha)
+    day = julian_day
     filename = f'{fecha}090000-JPL-L4_GHRSST-SSTfnd-MUR-{scale}-v02.0-fv04.1.nc'
     src = f'https://www.ncei.noaa.gov/data/oceans'
     url = f'{src}/{temperature_type}/{processing_level}/{scale}/{institution}/{sst_type}/{year}/{day}/{filename}'
     directory = f'{folder}/{filename}'
     return url, directory
-
-
-def get_url_errdap():
-    min_lat = 25.933687
-    max_lat = 51.25798
-    min_lon = -158.980523
-    max_lon = -114.42687
-    src = f'https://coastwatch.pfeg.noaa.gov/erddap/griddap'
-    variable = 'sea_surface_temperature'
-    fecha = '2023-06-30T12:00:00Z'
-    url = f'{src}/nceiPH53sstd1day.nc?{variable}%5B({fecha})%5D%5B({max_lat}):({min_lat})%5D%5B({min_lon}):({max_lon})%5D&.draw=surface&.vars=longitude%7Clatitude%7Csea_surface_temperature&.colorBar=%7C%7C%7C%7C%7C&.bgColor=0xffccccff'
-    return url
 
 def descargar_archivo(url, directory):
     try:
@@ -44,12 +37,11 @@ def descargar_archivo(url, directory):
         print("\nDescarga completada.")
     except Exception as e:
         print(f"Error al descargar el archivo: {e}")
+        
+years = [2014, 2015, 2016, 2017,]
+julian_days = [ f'{i:03d}' for i in range(1, 121) ]
 
-url_erddap = get_url_errdap()
-print('url del erddapL ', url_erddap)
-
-url, folder= get_url_noaa(2014,11,1)
-
-print(url, folder)
-
-descargar_archivo(url, folder)
+for year in years:
+    for julian_day in julian_days:
+        url, folder = get_url_noaa(year, julian_day)
+        descargar_archivo(url, folder)
