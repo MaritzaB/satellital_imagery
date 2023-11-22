9311c6d42e240d3b5349eb90c9ca8be02ba86b7b
maritzée <amby.121192@gmail.com>
2023-11-21 20:43:38 -0600

    📝 Update function to get chlorophyl data... Successfully!!! ✨
    📝

--- src/get_erddap_data.py
+++ src/get_erddap_data.py
@@ -1,5 +1,6 @@
 import wget
 from datetime import datetime
+import os
 
 def descargar_archivo(url, directory):
     try:
@@ -13,31 +14,29 @@ def get_SST_data(year, month):
     '''
     Source: 
     '''
-    url = f'https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41mday.nc?sst%5B({year}-{month}-16T00:00:00Z)%5D%5B({min_lat}):({max_lat})%5D%5B({min_lon}):({max_lon})%5D&.draw=surface&.vars=longitude%7Clatitude%7Csst&.colorBar=%7C%7C%7C%7C%7C&.bgColor=0xffccccff'
-    descargar_archivo(url, 'sst')
-    
+    url = f'https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdHadISST.nc?sst%5B(2018-01-16T12:00:00Z):1:(2018-04-16T12:00:00Z)%5D%5B(89.5):1:(-89.5)%5D%5B(-179.5):1:(179.5)%5D'
+    print(url)
+
+def get_chlc_data(year, month):
+    '''
+    Source: 'https://coastwatch.pfeg.noaa.gov/erddap/griddap/nesdisVHNSQchlaMonthly.graph?chlor_a'
+    '''
+    src = f'https://coastwatch.pfeg.noaa.gov/erddap/griddap/nesdisVHNSQchlaMonthly.nc?chlor_a'
+    url = f'{src}%5B({year}-{month}-01T12:00:00Z)%5D%5B(0.0)%5D%5B({min_lat}):({max_lat})%5D%5B({min_lon}):({max_lon})%5D&.draw=surface&.vars=longitude%7Clatitude%7Cchlor_a&.colorBar=%7C%7C%7C%7C%7C&.bgColor=0xffccccff'
+    print(url)
+    directory = f'chlc/{year}/'
+    os.makedirs(directory, exist_ok=True)
+    descargar_archivo(url, directory)
+
 min_lat = 23
 max_lat = 55
 min_lon = -163
 max_lon = -110
 
-year = '2019'
-month = '01'
+years = [2014, 2015, 2016, 2017, 2018]
+months = [f'{i:02d}' for i in range(1, 13) ]
 
-def get_CHL_data(year, month):
-    '''
-    Source:
-    - 2014: Chlorophyll, NOAA S-NPP VIIRS, Science Quality, Global 4km, Level 3, 2012-present, Monthly 
-    - 2015-2018: Chlorophyll a, North Pacific, NOAA VIIRS, 750m resolution, 2015-present (Monthly Composite)
-    '''
-    # url = f'https://coastwatch.pfeg.noaa.gov/erddap/griddap/nesdisVHNSQchlaMonthly.graph?chlor_a%5B(2014-01-01T12:00:00Z)%5D%5B(0.0)%5D%5B(54.99375):(23.006249999999994)%5D%5B(-162.99374999999998):(-110.00625)%5D&.draw=surface&.vars=longitude%7Clatitude%7Cchlor_a&.colorBar=%7C%7C%7C%7C%7C&.bgColor=0xffccccff'
-    url = f'https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdMBchla1day.nc?chlor_a%5B({year}-{month}-16T12:00:00Z)%5D%5B({min_lat}):({max_lat})%5D%5B({min_lon}):({max_lon})%5D&.draw=surface&.vars=longitude%7Clatitude%7Cchlor_a&.colorBar=%7C%7C%7C%7C%7C&.bgColor=0xffccccff'
-    descargar_archivo(url, 'chl')
-    
-def get_wind_speed_data(year, month):
-    '''
-    Source:
-    - FNMOC 10m Surface Winds, 360x181, Monthly
-    '''
-    #PENDIENTE
+for year in years:
+    for month in months:
+        get_chlc_data(year, month)
 
