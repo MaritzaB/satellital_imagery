1081bd74434a2967ef590dbf23de09e84f60269e
maritzée <amby.121192@gmail.com>
2024-02-21 21:05:55 -0600

    💫🔨🔧 update tools

--- src/get_erddap_data.py
+++ src/get_erddap_data.py
@@ -9,6 +9,23 @@ def descargar_archivo(url, filename):
         print("\nDescarga completada.")
     except Exception as e:
         print(f"Error al descargar el archivo: {e}")
+
+min_lat = 23
+max_lat = 55
+min_lon = -163
+max_lon = -110  
+        
+def get_sst_data(year, month):
+    '''
+    Source: 'https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST42mday.graph'
+    '''
+    src='https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST42mday.geotif?sst'
+    url=f'{src}%5B({year}-{month}-16T00:00:00Z)%5D%5B({min_lat}):({max_lat})%5D%5B({min_lon}):({max_lon})%5D&.draw=surface&.vars=longitude%7Clatitude%7Csst&.colorBar=%7C%7C%7C%7C%7C&.bgColor=0xffccccff'
+    print(url)
+    directory = f'sst/{year}/{month}/'
+    filename = f'{directory}/{year}{month}_MUR_SST.tif'
+    os.makedirs(directory, exist_ok=True)
+    descargar_archivo(url, filename)
         
 def get_chlc_data(year, month):
     '''
@@ -19,14 +36,10 @@ def get_chlc_data(year, month):
     print(url)
     directory = f'chlc/{year}/{month}/'
     os.makedirs(directory, exist_ok=True)
-    descargar_archivo(url, directory)
+    #descargar_archivo(url, directory)
 
-min_lat = 23
-max_lat = 55
-min_lon = -163
-max_lon = -110
 
-def get_wind_data(year, month, day, dist_surface=10):
+def get_wind_data(year, month, dist_surface=10):
     '''
     Source: FNMOC 10m Surface Winds, 360x181, Monthly, Lon+/-180
     https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdlasFnWind10_LonPM180.graph
@@ -36,15 +49,10 @@ def get_wind_data(year, month, day, dist_surface=10):
     '''
     latitude_ranges = f'%5D%5B({min_lat}):({max_lat})'
     longitude_ranges = f'%5D%5B({min_lon}):({max_lon})'
-    year_month = f'{year}-{month}-{day}T00:00:00Z'
-    #src = f'https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdlasFnWind{dist_surface}_LonPM180.nc'
-    #url =
-    #f'{src}?u_mean%5B({year}-{month}-01T00:00:00Z){latitude_ranges}{longitude_ranges}%5D,v_mean%5B({year}-{month}-01T00:00:00Z){latitude_ranges}{longitude_ranges}%5D&.draw=vectors&.vars=longitude%7Clatitude%7Cu_mean%7Cv_mean&.color=0x000000&.bgColor=0xffccccff'
-    ### NAVGEM 10m Surface Wind Lon+/-180
-    src = f'https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdNavgem05D10mWind_LonPM180.nc'
-    url = f'{src}?wnd_ucmp_height_above_ground%5B({year_month})%5D%5B(10.0){latitude_ranges}{longitude_ranges}%5D,wnd_vcmp_height_above_ground%5B({year_month})%5D%5B(10.0){latitude_ranges}{longitude_ranges}%5D&.draw=vectors&.vars=longitude%7Clatitude%7Cwnd_ucmp_height_above_ground%7Cwnd_vcmp_height_above_ground&.color=0x000000&.bgColor=0xffccccff'
-    directory = f'wind/'
-    file_name = f'{directory}/{year}{month}{day}_FNMOC_10m_Surface_Winds.nc'
+    src = f'https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdlasFnWind{dist_surface}_LonPM180.nc'
+    url = f'{src}?u_mean%5B({year}-{month}-01T00:00:00Z){latitude_ranges}{longitude_ranges}%5D,v_mean%5B({year}-{month}-01T00:00:00Z){latitude_ranges}{longitude_ranges}%5D&.draw=vectors&.vars=longitude%7Clatitude%7Cu_mean%7Cv_mean&.color=0x000000&.bgColor=0xffccccff'
+    directory = f'wind/{year}/{month}/'
+    file_name = f'{directory}/{year}{month}_FNMOC_10m_Surface_Winds.nc'
     print(url)
     os.makedirs(directory, exist_ok=True)
     descargar_archivo(url, file_name)
@@ -54,14 +62,21 @@ months = [f'{i:02d}' for i in range(1, 13) ]
 days = [f'{i:02d}' for i in range(1, 32) ]
 
 #data = 'chlc'
-data = 'wind'
+#data = 'wind'
+data = 'sst'
 
 if data == 'chlc':
     for year in years:
         for month in months:
             get_chlc_data(year, month)
+            pause = input('Press enter to continue: ')
 elif data == 'wind':
     for year in years:
         for month in months:
-            for day in days:
-                get_wind_data(year, month, day)
+            get_wind_data(year, month)
+            pause = input('Press enter to continue: ')
+elif data == 'sst':
+    for year in years:
+        for month in months:
+            get_sst_data(year, month)
+            #pause = input('Press enter to continue: ')
