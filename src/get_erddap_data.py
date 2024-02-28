10e137deca4ea21b672fff8c07eee4ae20afbe94
maritzée <amby.121192@gmail.com>
2024-02-27 21:04:51 -0600

    💩 At least now i can resample my raster data 🎉

--- src/get_erddap_data.py
+++ src/get_erddap_data.py
@@ -14,6 +14,11 @@ min_lat = 23
 max_lat = 55
 min_lon = -163
 max_lon = -110  
+
+def create_directory(year, month):
+    directory = f'data/{year}/{month}'
+    os.makedirs(directory, exist_ok=True)
+    return directory
         
 def get_sst_data(year, month):
     '''
@@ -22,12 +27,10 @@ def get_sst_data(year, month):
     '''
     #src='https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdHadISST.nc?sst%'
     #url=f'{src}5B({year}-{month}-16T12:00:00Z)%5D%5B({min_lat}):({max_lat})%5D%5B({min_lon}):({max_lon})%5D&.draw=surface&.vars=longitude%7Clatitude%7Csst&.colorBar=%7C%7C%7C%7C%7C&.bgColor=0xffccccff'
-    src = 'https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41mday.nc?sst%'
+    src = 'https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41mday.geotif?sst%'
     url = f'{src}5B({year}-{month}-16T00:00:00Z)%5D%5B({min_lat}):({max_lat})%5D%5B({min_lon}):({max_lon})%5D&.draw=surface&.vars=longitude%7Clatitude%7Csst&.colorBar=%7C%7C%7C%7C%7C&.bgColor=0xffccccff'
-    print(url)
-    directory = f'sst/{year}/{month}/'
-    filename = f'{directory}/{year}{month}_jplMURSST41_.nc'
-    os.makedirs(directory, exist_ok=True)
+    directory = create_directory(year, month)
+    filename = f'{directory}/{year}_{month}_jplMURSST41_.tif'
     descargar_archivo(url, filename)
     
 def get_wind_data(year, month, dist_surface=10):
@@ -37,52 +40,55 @@ def get_wind_data(year, month, dist_surface=10):
     
     Source: FNMOC 20m Surface Winds, 360x181, Monthly, Lon+/-180
     https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdlasFnWind20_LonPM180.graph
+    
+    Source: Global Ocean Monthly Mean Sea Surface Wind and Stress from
+    Scatterometer and Model --BUENO-- 0.25 Degree
+    https://data.marine.copernicus.eu/product/WIND_GLO_PHY_CLIMATE_L4_MY_012_003/files?subdataset=cmems_obs-wind_glo_phy_my_l4_P1M_202211
     '''
     latitude_ranges = f'%5D%5B({min_lat}):({max_lat})'
     longitude_ranges = f'%5D%5B({min_lon}):({max_lon})'
-    src = f'https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdlasFnWind{dist_surface}_LonPM180.geotif'
+    src = f'https://coastwatch.pfeg.noaa.gov/erddap/griddap/erdlasFnWind{dist_surface}_LonPM180.nc'
     url = f'{src}?u_mean%5B({year}-{month}-01T00:00:00Z){latitude_ranges}{longitude_ranges}%5D,v_mean%5B({year}-{month}-01T00:00:00Z){latitude_ranges}{longitude_ranges}%5D&.draw=vectors&.vars=longitude%7Clatitude%7Cu_mean%7Cv_mean&.color=0x000000&.bgColor=0xffccccff'
-    directory = f'wind/{year}/{month}/'
-    file_name = f'{directory}/{year}{month}_FNMOC_10m_Surface_Winds.tif'
-    print(url)
-    os.makedirs(directory, exist_ok=True)
-    descargar_archivo(url, file_name)
+    directory = create_directory(year, month)
+    filename = f'{directory}/{year}_{month}_FNMOC_10m_Surface_Winds.nc'
+    descargar_archivo(url, filename)
         
 def get_chlc_data(year, month):
     '''
     Source: 'https://coastwatch.pfeg.noaa.gov/erddap/griddap/nesdisVHNSQchlaMonthly.graph?chlor_a'
     '''
-    src = f'https://coastwatch.pfeg.noaa.gov/erddap/griddap/nesdisVHNSQchlaMonthly.nc?chlor_a'
+    src = f'https://coastwatch.pfeg.noaa.gov/erddap/griddap/nesdisVHNSQchlaMonthly.geotif?chlor_a'
     url = f'{src}%5B({year}-{month}-01T12:00:00Z)%5D%5B(0.0)%5D%5B({min_lat}):({max_lat})%5D%5B({min_lon}):({max_lon})%5D&.draw=surface&.vars=longitude%7Clatitude%7Cchlor_a&.colorBar=%7C%7C%7C%7C%7C&.bgColor=0xffccccff'
     print(url)
-    directory = f'chlc/{year}/{month}/'
-    filename = f'{directory}/{year}{month}_NESDIS_VHNSQ_chla.nc'
-    os.makedirs(directory, exist_ok=True)
+    directory = create_directory(year, month)
+    filename = f'{directory}/{year}_{month}_NESDIS_VHNSQ_chla.tif'
     descargar_archivo(url, filename)
 
+def process_data(process_function, years, months):
+    for year in years:
+        for month in months:
+            process_function(year, month)
+            # pause = input('Press enter to continue: ')
 
+def process_chlc_data(year, month):
+    get_chlc_data(year, month)
 
+def process_wind_data(year, month):
+    get_wind_data(year, month)
+
+def process_sst_data(year, month):
+    get_sst_data(year, month)
+
+data_processing_functions = {
+    'chlc': process_chlc_data,
+    'wind': process_wind_data,
+    'sst': process_sst_data
+}
 
 years = [2014, 2015, 2016, 2017, 2018]
 months = [f'{i:02d}' for i in range(1, 13) ]
-#days = [f'{i:02d}' for i in range(1, 32) ]
+datatype = ['chlc', 'sst']
 
-#data = 'chlc'
-#data = 'wind'
-data = 'sst'
-
-if data == 'chlc':
-    for year in years:
-        for month in months:
-            get_chlc_data(year, month)
-            #pause = input('Press enter to continue: ')
-elif data == 'wind':
-    for year in years:
-        for month in months:
-            get_wind_data(year, month)
-            #pause = input('Press enter to continue: ')
-elif data == 'sst':
-    for year in years:
-        for month in months:
-            get_sst_data(year, month)
-            #pause = input('Press enter to continue: ')
+for data in datatype:
+    if data in data_processing_functions:
+        process_data(data_processing_functions[data], years, months)
---
+++ src/resampling_raster.py
@@ -0,0 +1,54 @@
+import xarray as xr
+from rasterio.warp import reproject, Resampling, calculate_default_transform
+import rasterio
+
+def reproj_match(infile, match, outfile):
+    """Reproject a file to match the shape and projection of existing raster. 
+    
+    Parameters
+    ----------
+    infile : (string) path to input file to reproject
+    match : (string) path to raster with desired shape and projection 
+    outfile : (string) path to output file tif
+    """
+    # open input
+    with rasterio.open(infile) as src:
+        src_transform = src.transform
+        
+        # open input to match
+        with rasterio.open(match) as match:
+            dst_crs = match.crs
+            
+            # calculate the output transform matrix
+            dst_transform, dst_width, dst_height = calculate_default_transform(
+                src.crs,     # input CRS
+                dst_crs,     # output CRS
+                match.width,   # input width
+                match.height,  # input height 
+                *match.bounds,  # unpacks input outer boundaries (left, bottom, right, top)
+            )
+
+        # set properties for output
+        dst_kwargs = src.meta.copy()
+        dst_kwargs.update({"crs": dst_crs,
+                           "transform": dst_transform,
+                           "width": dst_width,
+                           "height": dst_height,
+                           "nodata": 0})
+        print("Coregistered to shape:", dst_height,dst_width,'\n Affine',dst_transform)
+        # open output
+        with rasterio.open(outfile, "w", **dst_kwargs) as dst:
+            # iterate through bands and write using reproject function
+            for i in range(1, src.count + 1):
+                reproject(
+                    source=rasterio.band(src, i),
+                    destination=rasterio.band(dst, i),
+                    src_transform=src.transform,
+                    src_crs=src.crs,
+                    dst_transform=dst_transform,
+                    dst_crs=dst_crs,
+                    resampling=Resampling.nearest)
+                
+match_dataset = xr.open_dataset('data/winds/cmems_obs-wind_glo_phy_my_l4_P1M_201401_clipped.nc')
+input_file = xr.open_dataset('data/2014/01/2014_01_jplMURSST41_.nc')
+outfile = 'data/2014/01/2014_01_jplMURSST41_resampled.nc'
