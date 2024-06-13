acb78cae58c0cc7bd77d8065be8223bd38abd4f7
maritzée <amby.121192@gmail.com>
2024-06-12 19:04:51 -0600

    💫💫 Update script to resample data

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 download:
 	yes | python3 src/get_data.py
+
+clean:
+	rm -rf resampled_data/
--- src/resample_raster.py
+++ src/resample_raster.py
@@ -11,7 +11,7 @@ def make_dir(directory):
 def write_raster(netcdf, new_directory):
     ds = rxr.open_rasterio(netcdf)
     for var in ds.data_vars:
-        print(var)
+        #print(f'Writing {var} to raster')
         ds[var].rio.to_raster(f'{new_directory}{var}.tif')
       
 def verify_shape(input_shape, reference_shape):
@@ -20,31 +20,56 @@ def verify_shape(input_shape, reference_shape):
     else:
         return False
 
-def resample_raster(input_file, reference_file, new_directory):
-    input_ds = rxr.open_rasterio(input_file)
-    reference_ds = rxr.open_rasterio(reference_file)
+def resample_sst_raster(input_file, reference_file, new_directory):
     CRS = 'EPSG:4326'
-    input_ds = input_ds.rio.write_crs(CRS)
+    reference_ds = rxr.open_rasterio(reference_file)
     reference_ds = reference_ds.rio.write_crs(CRS)
+    input_ds = rxr.open_rasterio(input_file)
+    input_ds = input_ds.rio.write_crs(CRS)
     reprojected = input_ds.rio.reproject_match(reference_ds, resampling=Resampling.average)
     reprojected = reprojected.rio.write_crs(CRS)
     output_file = input_ds.name + '_resampled.tif'
-    print(output_file)
     if verify_shape(reprojected.shape, reference_ds.shape):
         reprojected.rio.to_raster(f'{new_directory}{output_file}')
-        print('Shapes match')
+        #print('SST and Wind shapes match')
     else:
-        print('Shapes do not match')
+        print('SST and Wind shapes doesnt match')
+        return False
+    
+def resample_chla_raster(input_file, reference_file, new_directory):
+    CRS = 'EPSG:4326'
+    reference_ds = rxr.open_rasterio(reference_file)
+    reference_ds = reference_ds.rio.write_crs(CRS)
+    input_ds = xr.open_dataset(input_file)
+    input_ds = input_ds['chlor_a'].isel(altitude=0)
+    input_ds = input_ds.rio.write_crs(CRS)
+    reprojected = input_ds.rio.reproject_match(reference_ds, resampling=Resampling.average)
+    reprojected.attrs.pop('grid_mapping', None)
+    reprojected = reprojected.rio.write_crs(CRS)
+    output_file = input_ds.name + '_resampled.tif'
+    if verify_shape(reprojected.shape, reference_ds.shape):
+        reprojected.rio.to_raster(f'{new_directory}{output_file}')
+        #print('Chla and Wind shapes match')
+    else:
+        print('Chla and Wind shapes doesnt match')
         return False
 
-year = '2014'
-month = '01'
-dir = f'data/{year}/{month}/'
-wind_file = f'{dir}cmems_obs-wind_glo_phy_my_l4_P1M_201401.nc'
-new_dir = f'resampled_data/{year}/{month}/'
-match_file = f'{new_dir}eastward_wind.tif'
-sst_file = f'{dir}2014_01_jplMURSST41_.nc'
+def downsample_all_files(year, month):
+    dir = f'data/{year}/{month}/'
+    wind_file = f'{dir}cmems_obs-wind_glo_phy_my_l4_P1M_{year}{month}.nc'
+    sst_file = f'{dir}{year}_{month}_jplMURSST41_.nc'
+    chla_file = f'{dir}{year}_{month}_NESDIS_VHNSQ_chla.nc'
+    new_dir = f'resampled_data/{year}/{month}/'
+    match_file = f'{new_dir}eastward_wind.tif'
+    make_dir(new_dir)
+    write_raster(wind_file, new_dir)
+    resample_sst_raster(sst_file, match_file, new_dir)
+    resample_chla_raster(chla_file, match_file, new_dir)
+    
+years = [ year for year in range(2014, 2018)]
+months = [f'{i:02d}' for i in range(1, 13)]
 
-make_dir(new_dir)
-write_raster(wind_file, new_dir)
-resample_raster(sst_file, match_file, new_dir)
+for year in years:
+    for month in months:
+        print(f'Processing {year}-{month}')
+        downsample_all_files(year, month)
